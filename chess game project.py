91f6f1a5b955e4ed9b7a9027b3aab5e458c1661d
Martin896 <54917958+Martin896@users.noreply.github.com>
2023-01-03 12:46:47 -0800

    Add files via upload

---
+++ chess game project.py
@@ -0,0 +1,22 @@
+import sys 
+from PyQt5.Qtwidgets import QApplication, QMainwindow, Qwidget, QgridLayout, Qlabel
+from PyQt5.QtGui import Qpixmap
+
+class ChessGame(QMainwindow):
+    def __init__(self):
+        super().__int__()
+        
+        #set up main window 
+        self.setWindowTitle("Chess Game")
+        self.setGeometry(100, 100, 500, 500)
+        #creating a central widget to hold the chess board
+        central_widget = QWidget(self)
+        self.setCentralWidget(cental_widget)
+        #creating a grid layout for the chess board
+        grid_layout = QGridLayout(Central_widget)
+
+        #adding labels for the chess board 
+        self.squares =[]
+        for row in range(8):
+            self.square.append([])
+            
